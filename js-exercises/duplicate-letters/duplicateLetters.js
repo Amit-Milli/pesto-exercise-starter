505f18e97a8c76ead55c236a95335c889681978f
Amit-Milli <37293615+Amit-Milli@users.noreply.github.com>
2020-03-03 20:14:05 +0530

    Update duplicateLetters.js

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,6 +1,20 @@
 
-function duplicateLetters(...args) {
-  return args;
+function duplicateLetters(str) {
+  if(typeof str === 'string') {
+      const stringArr = str.split('');
+      const verificationObj = {};
+      for(let i = 0; i < stringArr.length; i++) {
+          if(stringArr[i] !== ' ') {
+            verificationObj[stringArr[i]] = verificationObj.hasOwnProperty(stringArr[i]) ? verificationObj[stringArr[i]] + 1 : 1;
+          }
+      }
+      const maxValue = Object.values(verificationObj).length > 0 ? Math.max(...Object.values(verificationObj)) : 0;
+      return maxValue <= 1 ? false : maxValue;  
+    } else {
+      // throw 'Invalid argument';
+      // returning string for testing purpose else would have thrown an exception
+      return 'Invalid argument';
+    }
 }
 
 export {
