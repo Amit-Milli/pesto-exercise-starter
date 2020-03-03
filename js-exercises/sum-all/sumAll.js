3a161e42cc05daaee63dd289cbe14c29ab1e0532
Amit-Milli <37293615+Amit-Milli@users.noreply.github.com>
2020-03-03 20:08:29 +0530

    Update sumAll.js

--- js-exercises/sum-all/sumAll.js
+++ js-exercises/sum-all/sumAll.js
@@ -1,5 +1,18 @@
-function sumAll() {
-  
+function sumAll(...args) {
+  if(args.length === 2 && args.filter(d => typeof d === "number").length === 2) {
+    const valueArray = args.sort();
+    let i = valueArray[0];
+    let result = valueArray[0];
+    do {
+      i = i + 1;
+      result = result + i;
+    } while (i < valueArray[1]);
+    return result;
+  } else {
+    // throw 'Invalid argument';
+    // returning string for testing purpose else would have thrown an error
+    return 'Invalid arguments';
+  }
 }
 
 export {
