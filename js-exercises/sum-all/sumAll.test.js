ee5441ea1b547e07ce003b033e6657ab550460ba
Amit-Milli <37293615+Amit-Milli@users.noreply.github.com>
2020-03-03 20:10:37 +0530

    Update sumAll.test.js

--- js-exercises/sum-all/sumAll.test.js
+++ js-exercises/sum-all/sumAll.test.js
@@ -9,5 +9,10 @@ describe('sumAll', () => {
     expect(sumAll([4, 1])).toBe(10);
     expect(sumAll([5, 10])).toBe(45);
     expect(sumAll([10, 5])).toBe(45);
+    expect(sumAll([-10, 10])).toBe(0);
+    expect(sumAll([-1, 10])).toBe(54);
+    expect(sumAll([10, -1])).toBe(54);
+    expect(sumAll([10, 'ff'])).toBe('Invalid arguments');
+    expect(sumAll({},[])).toBe('Invalid arguments');
   });
 });
