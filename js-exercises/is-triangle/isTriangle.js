d8c62358ae09c7e3e2a048474fc51a485665d910
Amit-Milli <37293615+Amit-Milli@users.noreply.github.com>
2020-03-03 20:11:19 +0530

    Update isTriangle.js

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,5 +1,14 @@
-function isTriangle() {
-
+function isTriangle(...args) {
+  if(args.length === 3 && args.filter(d => typeof d === "number").length === 3) {
+    const [a, b, c] = args;
+    return (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) ||
+    (Math.pow(b, 2) + Math.pow(c, 2) === Math.pow(a, 2)) ||
+    (Math.pow(c, 2) + Math.pow(a, 2) === Math.pow(b, 2));
+  } else {
+    // throw 'Invalid argument';
+    // returning string for testing purpose else would have thrown an error
+    return 'Invalid arguments';
+  }
 }
 
 export {
